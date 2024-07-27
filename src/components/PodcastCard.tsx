c946afdf54c9dd7b3050a16f00b979ca08816dc8
Ayush Singh <asayushsingh80@gmail.com>
2024-07-27 22:08:28 +0530

    ui: add PodcastCard

---
+++ src/components/PodcastCard.tsx
@@ -0,0 +1,34 @@
+import Image from "next/image";
+
+interface PodcastCardProps {
+  podcastID: number;
+  imgURL: string;
+  description: string;
+  title: string;
+}
+
+export default function PodcastCard({
+  imgURL,
+  description,
+  title,
+}: PodcastCardProps) {
+  return (
+    <div className="cursor-point">
+      <figure className="flex flex-col gap-2">
+        <Image
+          src={imgURL}
+          width={174}
+          height={174}
+          alt={title}
+          className="aspect-square h-fite w-full rounded-xl 2xl:size-[200px]"
+        />
+        <div className="flex flex-col">
+          <h1 className="text-16 truncate font-bold text-white-1">{title}</h1>
+          <h2 className="text-12 truncate font-normal capitalize text-white-4">
+            {description}
+          </h2>
+        </div>
+      </figure>
+    </div>
+  );
+}
